be12469e3e7b93abdb3586c9b51f916f18c9190a
Josh Grunzweig <jgrunzweig@gmail.com>
2012-03-08 15:16:16 -0600

    Removed some unnecessary fields in the Tag table

--- lib/ptj/model/tag.rb
+++ lib/ptj/model/tag.rb
@@ -7,12 +7,6 @@ module PTJ
     # A textual tag name
     property :tag,          String, :key => true
 
-    # An optional description of the tag.
-    property :description,  Text
-
-    property :created_at,       DateTime, :writer => :private 
-    property :updated_at,       DateTime, :writer => :private 
-
     has n, :passwords,    :through => Resource
 
   end
